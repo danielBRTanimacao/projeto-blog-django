e81f6e910c01942219b08994a9e4a38e04ea0ad7
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-06-20 15:14:29 -0300

    usando validators e sobrescrevendo metodo save

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from utils.model_validator import validate_png
+from utils.images import resize_image
 
 # Create your models here.
 class MenuLink(models.Model):
@@ -41,8 +42,19 @@ class SiteSetup(models.Model):
         upload_to='assets/favicon/%Y/%m/',
         blank=True,
         default='',
-        validators=[]
+        validators=[validate_png],
     )
+
+    def save(self, *args, **kwargs):
+        current_favicon_name = str(self.favicon.name)
+        super().save(*args, **kwargs)
+        favicon_changed = False
+
+        if self.favicon:
+            favicon_changed = current_favicon_name != self.favicon.name
+
+        if favicon_changed:
+            resize_image(self.favicon, 32)
 
     def __str__(self):
         return self.title
