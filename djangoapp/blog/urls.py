9fa7f3dd7fd9b56263d74b9d168fec1f2af8a85c
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-06-25 08:55:55 -0300

    concertando as path url

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from blog import views
+from blog import views, post, page
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post/', post, name='post'),
+    path('page/', page, name='page'),
 ]
