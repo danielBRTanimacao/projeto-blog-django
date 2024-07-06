a583f8ccc709986df540a6ffc2e0b0105550fb8a
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-07-06 09:38:53 -0300

    docker adicionando models

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -20,3 +20,54 @@ class Tag(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.name, 4)
         return super.save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.name
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(
+        unique=True,
+        default=None,
+        null=True,
+        blank=True,
+        max_length=255
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.name, 4)
+        return super.save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.name
+
+    
+class Page(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(
+        unique=True,
+        default=None,
+        null=True,
+        blank=True,
+        max_length=255
+    )
+    is_published = models.BooleanField(default=False)
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super.save(*args, **kwargs)
+    
+    def __str__(self) -> str:
+        return self.title
