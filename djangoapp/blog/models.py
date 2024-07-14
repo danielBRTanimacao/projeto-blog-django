da277f7bd0b93324276b416045fddc5f0ea94fde
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-07-14 09:32:02 -0300

    pillow e slug save

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from utils.rands import slugify_new
+from utils.images import resize_image
 
 # Create your models here.
 class Tag(models.Model):
@@ -42,7 +43,18 @@ class Category(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify_new(self.name, 4)
-        return super.save(*args, **kwargs)
+        
+        current_cover_name = str(self.cover.name)
+        super_save = super().save(*args, **kwargs)
+        cover_changed = False
+
+        if self.cover:
+            cover_changed = current_cover_name != self.cover.name
+
+        if cover_changed:
+            resize_image(self.cover, 900, True, 70)
+
+        return super_save
     
     def __str__(self) -> str:
         return self.name
