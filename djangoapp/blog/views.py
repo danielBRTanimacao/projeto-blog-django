57ea5acbc62333c3a924313f858df01dec0d4526
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-07-18 13:46:18 -0300

    init parte interna do blog

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 def index(request):
@@ -10,8 +11,46 @@ def page(request):
         'blog/pages/page.html'
     )
 
-def post(request):
+def post(request, slug):
+    post = Post.objects.get_published().filter(slug=slug).first()
+
     return render(
         request,
-        'blog/pages/post.html'
+        'blog/pages/post.html',
+        {
+            'post': post
+        }
+    )
+
+def created_by(request, author_pk):
+    posts = Post.objects.get_published()\
+        .filter(created_by__pk=author_pk)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def category(request, slug):
+    posts = Post.objects.get_published()\
+        .filter(category__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
     )
